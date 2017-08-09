87a0b16d9dff4672d7913824564d648700471203
Alistair Kroie <Alistair.Kroie@live.com>
2017-08-09 17:56:37 +0100

    js stuff

---
+++ js/main.js
@@ -0,0 +1,404 @@
+$('#open-popup-samsung').magnificPopup({
+    items: 
+
+          {
+        src: 'http://vimeo.com/89978682',
+        type: 'iframe' // this overrides default type
+      },
+});
+
+
+$('#open-popup-spill-seekers').magnificPopup({
+    items: 
+
+          {
+        src: 'http://vimeo.com/70598851',
+        type: 'iframe' // this overrides default type
+      },
+});
+
+$('#open-popup-code-camp').magnificPopup({
+    items: 
+
+          {
+        src: 'http://vimeo.com/97398013',
+        type: 'iframe' // this overrides default type
+      },
+});
+
+$('#open-popup-camp-creators').magnificPopup({
+    items: 
+
+          {
+        src: 'http://vimeo.com/97398014',
+        type: 'iframe' // this overrides default type
+      },
+});
+
+$('#open-popup-toy-fair').magnificPopup({
+    items: 
+
+          {
+        src: 'http://vimeo.com/70164634',
+        type: 'iframe' // this overrides default type
+      },
+});
+
+
+$('#open-popup-mnick').magnificPopup({
+    items: 
+
+          {
+        src: 'http://vimeo.com/89993013',
+        type: 'iframe' // this overrides default type
+      },
+});
+
+
+$('#open-popup-dare-off').magnificPopup({
+    items: 
+
+          {
+        src: 'http://vimeo.com/71950423',
+        type: 'iframe' // this overrides default type
+      },
+});
+
+
+$('#open-popup-slimefest-show').magnificPopup({
+    items: 
+
+          {
+        src: 'http://vimeo.com/67210879',
+        type: 'iframe' // this overrides default type
+      },
+});
+
+$('#open-popup-slimefest-2013').magnificPopup({
+    items: 
+
+          {
+        src: 'http://vimeo.com/89988795',
+        type: 'iframe' // this overrides default type
+      },
+});
+
+$('#open-popup-cross-platform').magnificPopup({
+    items: 
+
+          {
+        src: 'http://vimeo.com/72017887',
+        type: 'iframe' // this overrides default type
+      },
+});
+
+
+
+$('#open-popup-kctv').magnificPopup({
+    items: 
+
+          {
+        src: 'http://vimeo.com/86486353',
+        type: 'iframe' // this overrides default type
+      },
+});
+
+
+$('#open-popup-kca').magnificPopup({
+    items: 
+
+          {
+        src: 'http://vimeo.com/86486354',
+        type: 'iframe' // this overrides default type
+      },
+});
+
+$('#open-popup-co-gvb').magnificPopup({
+    items: 
+
+          {
+        src: 'http://vimeo.com/86488185',
+        type: 'iframe' // this overrides default type
+      },
+});
+
+$('#open-popup-co-wt').magnificPopup({
+    items: 
+
+          {
+        src: 'http://vimeo.com/72017888',
+        type: 'iframe' // this overrides default type
+      },
+});
+
+$('#open-popup-sf-teasers').magnificPopup({
+    items: 
+
+          {
+        src: 'http://vimeo.com/86486355',
+        type: 'iframe' // this overrides default type
+      },
+});
+
+$('#open-popup-sf-teasers2').magnificPopup({
+    items: 
+
+          {
+        src: 'http://vimeo.com/89987459',
+        type: 'iframe' // this overrides default type
+      },
+});
+
+$('#open-popup-itunes').magnificPopup({
+    items: 
+
+          {
+        src: 'http://vimeo.com/89993014',
+        type: 'iframe' // this overrides default type
+      },
+});
+
+$('#open-popup-btr').magnificPopup({
+    items: 
+
+          {
+        src: 'http://vimeo.com/89989760',
+        type: 'iframe' // this overrides default type
+      },
+});
+
+$('#open-popup-cross-wishcast').magnificPopup({
+    items: 
+
+          {
+        src: 'http://vimeo.com/72018086',
+        type: 'iframe' // this overrides default type
+      },
+});
+
+
+$('#open-popup-cross-old-vids').magnificPopup({
+    items: 
+
+          {
+        src: 'http://vimeo.com/86316738',
+        type: 'iframe' // this overrides default type
+      },
+});
+
+$('#open-popup-judge').magnificPopup({
+    items: 
+
+          {
+        src: 'http://vimeo.com/86816673',
+        type: 'iframe' // this overrides default type
+      },
+});
+
+$('#open-popup-die-2nd-time').magnificPopup({
+    items: 
+
+          {
+        src: 'http://vimeo.com/90111510',
+        type: 'iframe' // this overrides default type
+      },
+});
+
+$('#open-popup-journey').magnificPopup({
+    items: [
+
+    {
+      src: 'img/journey/journey-queen.jpg'
+
+    },
+
+    {
+      src: 'img/journey/journey-god.jpg'
+    },
+
+    {
+      src: 'img/journey/journey-city.jpg'
+    },
+
+    {
+      src: 'img/journey/journey-ruins.jpg'
+    },
+
+    {
+      src: 'img/journey/journey-tiger.jpg'
+    },
+
+    {
+      src: 'img/journey/me-tiger.jpg'
+    },
+
+        ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is default type
+});
+
+
+$('#open-popup-tag').magnificPopup({
+    items: 
+
+    {
+        src: 'http://www.youtube.com/watch?v=hnoIFKZSOdQ',
+        type: 'iframe' // this overrides default type
+    },
+});
+
+$('#open-popup-prowl').magnificPopup({
+    items: 
+
+    {
+        src: 'http://www.youtube.com/watch?v=g1dm1KG2YNg',
+        type: 'iframe' // this overrides default type
+    },
+});
+
+
+$('#open-popup-mc92').magnificPopup({
+    items: [
+
+    {
+        src: 'http://vimeo.com/86816674',
+        type: 'iframe' // this overrides default type
+    },
+
+    {
+      src: 'img/mc92/mc92-tony2.jpg'
+
+    },
+
+    {
+      src: 'img/mc92/mc92-time-travel.jpg'
+    },
+
+    {
+      src: 'img/mc92/mc92-tony.jpg'
+    },
+
+    {
+      src: 'img/mc92/mc92-portal.jpg'
+    },
+
+    {
+      src: 'img/mc92/mc92-mg.jpg'
+    },
+
+        ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is default type
+});
+
+$('#open-popup-blitz').magnificPopup({
+    items: [
+
+    {
+      src: 'img/blitz/1.jpg'
+
+    },
+
+    {
+      src: 'img/blitz/2.jpg'
+    },
+
+    {
+      src: 'img/blitz/3.jpg'
+    },
+
+    {
+      src: 'img/blitz/4.jpg'
+    },
+
+    {
+      src: 'img/blitz/5.jpg'
+    },
+
+    {
+      src: 'img/blitz/6.jpg'
+    },
+
+    {
+      src: 'img/blitz/7.jpg'
+    },
+
+    {
+      src: 'img/blitz/8.jpg'
+    },
+
+    {
+      src: 'img/blitz/9.jpg'
+    },
+
+        ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is default type
+});
+
+
+$('#open-popup-firstplay').magnificPopup({
+    items: [
+
+    {
+      src: 'img/firstplay/first-play.jpg'
+
+    },
+
+    {
+      src: 'img/firstplay/first-play2.jpg'
+    },
+
+    {
+      src: 'img/firstplay/first-play-3.jpg'
+    },
+
+
+        ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is default type
+});
+
+$('#open-popup-toon-party').magnificPopup({
+    items: 
+
+          {
+        src: 'http://vimeo.com/97398012',
+        type: 'iframe' // this overrides default type
+      },
+});
+
+$('#open-popup-appy').magnificPopup({
+    items: 
+
+          {
+        src: 'http://vimeo.com/115684427',
+        type: 'iframe' // this overrides default type
+      },
+});
+
+$('#open-popup-nick-new').magnificPopup({
+    items: 
+
+          {
+        src: 'http://vimeo.com/114234793',
+        type: 'iframe' // this overrides default type
+      },
+});
+
+$('#open-popup-kids-tech').magnificPopup({
+    items: 
+
+          {
+        src: 'http://vimeo.com/114321996',
+        type: 'iframe' // this overrides default type
+      },
+});
